f3327e7ab8f2a2f346a2835e05b55d987a8b73b8
Ethan-Edmond <eedmond@uccs.edu>
2021-05-10 13:14:35 -0600

    Step 2 of Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -41,6 +41,7 @@ import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Spinner() {
 /* STEP 1 */
+  const [spinnerOn, setSpinnerOn] = useState(true);
 
   const toggleSpinner = () => {
   /* STEP 4 */
@@ -50,7 +51,7 @@ export default function Spinner() {
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        spnnerOn && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
         Hide Spinner {/* STEP 3 */}
