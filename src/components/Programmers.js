2526a78668ef58bf0926831df2439c5eb2e0a39b
Ethan-Edmond <eedmond@uccs.edu>
2021-05-10 14:19:56 -0600

    Changed false to featured

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -64,7 +64,7 @@ export default function Programmers() {
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          false
+          (featured)
             ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
